e03a355bb982d7c1b76c750f8a01cd04dc91f5bb
blueboyhi <blueboy1981616@gmail.com>
2012-09-03 22:24:54 +0800

    test
    test

---
+++ Hotel/Hotel/DemoTest.h
@@ -0,0 +1,13 @@
+//
+//  DemoTest.h
+//  Hotel
+//
+//  Created by apple on 12-8-26.
+//  Copyright (c) 2012年 blueboyhi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoTest : NSObject
+
+@end
---
+++ Hotel/Hotel/DemoTest.m
@@ -0,0 +1,13 @@
+//
+//  DemoTest.m
+//  Hotel
+//
+//  Created by apple on 12-8-26.
+//  Copyright (c) 2012年 blueboyhi. All rights reserved.
+//
+
+#import "DemoTest.h"
+
+@implementation DemoTest
+
+@end
